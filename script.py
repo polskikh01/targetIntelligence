25e0573e21ecf2ae4a0c3a0c77134ad37fb26a70
KluevJakov <66343904+KluevJakov@users.noreply.github.com>
2021-08-21 21:43:07 +0400

    Add files via upload
    advanced options

--- script.py
+++ script.py
@@ -5,46 +5,104 @@ import time
 from datetime import datetime
 
 #['AddedOnDate', 'Mac', 'RadarId', 'Face', 'AdmTypeBit', 'AddedOnTick', 'FrameOid']
-col = ['FrameOid', 'Mac', 'AddedOnTick']
-test = pandas.read_parquet("E:\dataset\RowData\crowd\player=40\month=2021-1.parquet", engine = "fastparquet", columns = col)
+#['OidColumn', 'PlayerUUID', 'BillingDate', 'MediaItemName', 'Duration', 'BookingId', 'LogFileName', 'Src', 'MediaItemId', 'advertisementId', 'MacCountAll', 'MacCountStaticOnly', 'DateTimeInTick', 'DateTimeOutTick']
+colPlayerLog = ['OidColumn', 'MediaItemName', 'advertisementId', 'MacCountAll']
+colCrowd = ['AddedOnDate', 'Mac', 'RadarId', 'Face', 'AdmTypeBit', 'AddedOnTick', 'FrameOid']
 
-count = 0
+dirsCrowd = ["E:\dataset\RowData\crowd\player=40\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=257\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=258\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=259\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=260\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=261\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=262\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=263\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=264\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=265\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=267\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=268\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=269\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=270\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=271\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=272\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=274\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=333\month=2020-11.parquet",
+        "E:\dataset\RowData\crowd\player=403\month=2020-11.parquet"]
 
-for i in range(0,test.count()[0]):
-    if test.loc[i].get('FrameOid') == str(5704191933):
-        count+=1
-        print(test.loc[i].get('FrameOid') + str(" !!!"))
-    print(test.loc[i].get('FrameOid'))
+dirsPlayerLog = ["E:\dataset\RowData\player_log\player=26\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=28\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=256\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=257\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=258\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=271\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=278\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=280\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=281\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=282\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=342\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=408\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=411\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=1547\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=1548\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=1571\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=2609\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=2619\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=2629\month=2020-11.parquet",
+        "E:\dataset\RowData\player_log\player=2651\month=2020-11.parquet"]
 
-print(count)
+
+for i in range(0, len(dirsPlayerLog)):
+    test2 = pandas.read_parquet(dirsPlayerLog[i], engine = "fastparquet", columns = colPlayerLog)
+    print(test2.loc[test2['advertisementId'] == 15715]['MacCountAll'].sum())
+
+
+
+#ПРИГОДИТСЯ
+#for i in range(0, 100):
+#    print(test1.loc[i])
+
+#test2 = pandas.read_parquet(dirs[1], engine = "fastparquet", columns = col2)
+#print(test2.loc[0])
+
+#print("\n")
+
+#test1 = pandas.read_parquet("E:\dataset\RowData\player_log\player=257\month=2020-11.parquet", engine = "fastparquet", columns = col1)
+#print(test1.loc[0])
+
+#for i in range(0, len(dirs)):
+#    test2 = pandas.read_parquet(dirs[i], engine = "fastparquet", columns = col2)
+#    print(test2.loc[test2['FrameOid'] == '5088033011'])
+
+#test2 = pandas.read_parquet("E:\dataset\RowData\crowd\player=258\month=2021-6.parquet", engine = "fastparquet", columns = col2)
+
+#print(datetime.utcfromtimestamp(int(test.loc[0]['AddedOnTick'])/1000).strftime('%Y-%m-%d %H:%M:%S'))
+#print(test2.loc[])
+#print(test2.loc[test2['FrameOid'] == '7358818680'])
+#print(test.loc[test['FrameOid'] == test.loc[testFrameOid]['FrameOid']].count()['FrameOid'])
+
+
+
+
+
+
+
+
+
+
 
 
 #визуализация
 '''
-# Fixing random state for reproducibility
-np.random.seed(19680801)
+dictionary = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 5, 
+              12: 8, 13: 74, 14: 4, 15: 3, 16: 1, 17: 1, 18: 1, 19: 1, 20: 1, 21: 2,22: 10, 23: 100, 24: 24, 25: 26,
+              26: 45, 27: 56, 28: 56, 29: 45, 30: 34, 31: 26}
+plt.bar(list(dictionary.keys()), dictionary.values(), color='g')
 
-mu, sigma = 100, 15
-x = mu + sigma * np.random.randn(10000)
-
-# the histogram of the data
-n, bins, patches = plt.hist(x, 50, density=True, facecolor='g', alpha=0.75)
-
-plt.xlabel('OTS')
-plt.ylabel('Days of Month')
+plt.xlabel('Days of Month')
+plt.ylabel('OTS')
 plt.title('Histogram of OTS per days of mouth')
-plt.xlim(40, 160)
-plt.ylim(0, 0.03)
+plt.xlim(0, 31)
+plt.ylim(0, 1500)
 plt.grid(True)
 plt.show()
-'''
-
-#пересчёт datatime
-'''
-from datetime import datetime
-ts = int("1284101485")
-
-# if you encounter a "year is out of range" error the timestamp
-# may be in milliseconds, try `ts /= 1000` in that case
-print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
 '''
